70f473d7c3dac3b0b2bd29b9bcb8febd0145ae0c
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-09-25 13:06:39 +0300

    MVC06 Notes

---
+++ MVC04/Notes/Note.cs
@@ -0,0 +1,13 @@
+﻿//ViewBag and ViewData are used to pass data from the Controller to the View - From View To Partial View - From View To Layout
+// ViewBag is a dynamic object - ViewData is a dictionary object
+//TempData → when passing data across redirects (Controller → Controller).
+
+
+
+//Partial View 
+//views allowing you to avoid code duplication and keep your views clean and maintainable.
+
+//Service Life Time
+//Singleton : Created once and shared throughout the application's lifetime.
+//Scoped : Created once per client request (connection).
+//Transient : Created each time they are requested.

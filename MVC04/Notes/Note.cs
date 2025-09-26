7620023eca0bffc9d8aeafe8a937565e5e90e010
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-09-26 18:02:53 +0300

    Notes MVC07

--- MVC04/Notes/Note.cs
+++ MVC04/Notes/Note.cs
@@ -11,3 +11,26 @@
 //Singleton : Created once and shared throughout the application's lifetime.
 //Scoped    : Created once per client request (connection).
 //Transient : Created each time they are requested.
+
+
+
+//Unite of Work 
+//design pattern that ensures multiple operations (like inserts, updates, deletes) are executed in a single transaction
+
+
+//Using lazy initialization to defer the creation of an object until it is actually needed
+//
+
+
+//Attachment Service
+
+//1.Check Extension
+//2. Check Size 
+//3. Get Located Folder Path
+//4. Make Attachment Name Unique -- GUID
+//5. Get File Path
+//6. Create File Stream To Copy File [Unmanaged]
+//7.Use Stream To Copy File 
+//8. Return FileName To Store In Database 
+
+

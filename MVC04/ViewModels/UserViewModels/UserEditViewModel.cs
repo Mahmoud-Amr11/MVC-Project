e165e57cf8c32dd51f12af02174bbed4a0fbec79
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-10-02 15:10:17 +0300

    User Crud Operation

---
+++ MVC04/ViewModels/UserViewModels/UserEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC04.ViewModels.UserViewModels
+{
+    public class UserEditViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        [Display(Name = "First Name")]
+        public string FName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [Display(Name = "Last Name")]
+        public string LName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; }
+
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+
+}
---
+++ MVC04/ViewModels/UserViewModels/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC04.ViewModels.UserViewModels
+{
+    public class UserViewModel
+    {
+        public string Id{ get; set; }
+
+        [Display(Name = "First Name")]
+        public string FName { get; set; }
+        [Display(Name = "Last Name")]
+
+        public string LName { get; set; }
+
+        public string Email { get; set; }
+    }
+
+}
